b8c55fe2c5d79ba00db5d3a383aa6fac7b28ef6e
lakshanaa07 <148964858+lakshanaa07@users.noreply.github.com>
2023-10-30 18:01:07 +0530

    Update inter1.sql

--- models/example/inter1.sql
+++ models/example/inter1.sql
@@ -2,11 +2,12 @@ With
 {{config(materialized='view')}}
 Inter as(
 Select
-    {{dbt_utils.star(from=ref('stage3'))}},
-    {{dbt_utils.star(from=ref('stage4'))}}
+    {{dbt_utils.star(from=ref('stage3')) }},
+    {{dbt_utils.star(from=ref('stage4')) }}
 From
    {{ref('stage3') }}as t1
 Left join
-    {{ref('stage4')}} as t2
-On t1.id=t2.id)
+    {{ref('stage4') }} as t2
+On t1.id=t2.id
+    )
 Select * from Inter
